d0d129fa0969da5a8bd70fba1e3e16cbaf3b2028
Tomasz Borczyk <tborcz@gmail.com>
2017-11-23 03:22:09 +0100

    - RSA fully works

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ from rsaCrypto import *
 from openKeys import *
 from generateKeys import *
 
-# # encrypt
-# public = getPublic()
-# image = openImage('images/lena-greyscale-small.png')
-# # image.show()
-# pixels = getPixelArray(image)
+decision = input('Encrypt or decrypt?')
+if decision is 'e':
+    # encrypt
+    public = getPublic()
+    # image = openImage('images/lena-greyscale-small.png')
+    # # image.show()
+    # pixels = getPixelArray(image)
+    # print(pixels)
+    # strImg = str(pixels)
+    encryptedImg = encrypt('Lubie placki', public)
+    print(encryptedImg)
 
-# strImg = str(pixels)
-# encryptedImg = encrypt(strImg, public)
+    with open('test', 'w') as outfile:
+        outfile.write(str(encryptedImg))
 
-# with open('test', 'w') as outfile:
-#     outfile.write(str(encryptedImg))
-
-# decrypt
-private = getPrivate()
-encryptedImg = open('test', 'r').read()
-print(encryptedImg)
-decryptedStr = decrypt(int(encryptedImg), private)
-print(decryptedStr)
+if decision is 'd':
+    # decrypt
+    private = getPrivate()
+    encryptedImg = open('test', 'r').read()
+    print(encryptedImg)
+    decryptedStr = decrypt(int(encryptedImg), private)
+    print('asdasd', decryptedStr)
+    text = asciiToText(decryptedStr)
+    print(text)
--- rsaCrypto.py
+++ rsaCrypto.py
@@ -1,11 +1,26 @@
+def toHex(msg):
+    return ''.join(format(ord(c), 'x') for c in msg)
+
+def asciiToText(msg):
+    length, size = len(msg), 3
+    symbols = [ msg[i:i+size] for i in range(0, length, size)]
+    return ''.join(chr(int(s[0].strip('0') + s[1:])) for s in symbols)
+
+
 def encrypt(msg, publicKey):
     n, e = publicKey
-    m = int(''.join(str(ord(c)) for c in msg))
+    hex = toHex(msg)
+    # for c in msg:
+    #     print(str(ord(c)).zfill(3)) 
+    # m = int(''.join(str(ord(c)).zfill(3) for c in msg))
+    m = int(''.join(str(ord(c, 'x')) for c in msg))
     # m = 3
     print('message to ascii', m)
     return pow(m, e, n)
 
 def decrypt(cipher, privateKey):
     n, d = privateKey
-    print('d', d)
-    return pow(cipher, d, n)
+    decryptedVal = str(pow(cipher, d, n))
+    if (len(decryptedVal) % 3 != 0):
+        decryptedVal = '0' + decryptedVal
+    return decryptedVal
