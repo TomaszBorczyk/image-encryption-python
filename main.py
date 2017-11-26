d1e16e335a4b83cf992593d169d6b78c942c9ded
Tomasz Borczyk <tborcz@gmail.com>
2017-11-26 01:14:49 +0100

    - Added main loop

--- main.py
+++ main.py
@@ -9,51 +9,52 @@ from generateKeys import *
 from keysBase64 import *
 from PIL import Image
 
-decision = input('RSA or Caesar? (r/c), Encrypt or decrypt? Or Cesar?')
-if decision is 'r':
-    decision = input('Generate key, encrypt or decrypt? (g/e/d')
+while(True):
+    decision = input('RSA or Caesar? (r/c), Encrypt or decrypt? Or Cesar?')
+    if decision is 'r':
+        decision = input('Generate key, encrypt or decrypt? (g/e/d')
+            
+        # generate keys
+        if decision is 'g':
+            private, public = generateKeys()
+            encodeAndSavePrivate(private)
+            encodeAndSavePublic(public)
+
+        # encrypt
+        if decision is 'e':
+            openImage('images/lena-greyscale.png').show()
+            with open('images/lena-greyscale.png', "rb") as imageFile:
+                image64 = base64.b64encode(imageFile.read()).decode()
+                # image64 = image64.decode()
+
+            public = loadPublicKey()
+            encrypted = encryptLargeFile(image64, public)
+
+            with open('encryptedimg', 'w') as outfile:
+                outfile.write(str(encrypted))
+
+        # decrypt
+        if decision is 'd':
+            private = loadPrivateKey()
+
+            encrypted = open('encryptedimg', 'r').read()
+            decryptedVal = decryptLargeFile(encrypted, private)
+            decryptedBytes = base64.b64decode(decryptedVal)
+            
+            with open('images/new.png', 'wb') as outfile:
+                outfile.write(decryptedBytes)
+
+            openImage('images/new.png').show()
+
+    if decision is 'c':
+        decision = input('Generate, Encrypt or decrypt? (g/e/d)')
         
-    # generate keys
-    if decision is 'g':
-        private, public = generateKeys()
-        encodeAndSavePrivate(private)
-        encodeAndSavePublic(public)
-
-    # encrypt
-    if decision is 'e':
-        openImage('images/lena-greyscale.png').show()
-        with open('images/lena-greyscale.png', "rb") as imageFile:
-            image64 = base64.b64encode(imageFile.read()).decode()
-            # image64 = image64.decode()
-
-        public = loadPublicKey()
-        encrypted = encryptLargeFile(image64, public)
-
-        with open('encryptedimg', 'w') as outfile:
-            outfile.write(str(encrypted))
-
-    # decrypt
-    if decision is 'd':
-        private = loadPrivateKey()
-
-        encrypted = open('encryptedimg', 'r').read()
-        decryptedVal = decryptLargeFile(encrypted, private)
-        decryptedBytes = base64.b64decode(decryptedVal)
+        if decision is 'g':
+            caesarCreate()
         
-        with open('images/new.png', 'wb') as outfile:
-            outfile.write(decryptedBytes)
+        if decision is 'e':
+            cipher('encrypt', 'images/lena-greyscale.png', 'images/lena-encrypted-caesar.png')
+            
 
-        openImage('images/new.png').show()
-
-if decision is 'c':
-    decision = input('Generate, Encrypt or decrypt? (g/e/d)')
-    
-    if decision is 'g':
-        caesarCreate()
-    
-    if decision is 'e':
-        cipher('encrypt', 'images/lena-greyscale-small.png', 'images/lena-encrypted-caesar.png')
-        
-
-    if decision is 'd':
-        cipher('decrypt', 'images/lena-encrypted-caesar.png', 'images/lena-decrypted-caesar.png')
+        if decision is 'd':
+            cipher('decrypt', 'images/lena-encrypted-caesar.png', 'images/lena-decrypted-caesar.png')
